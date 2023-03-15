03c4f9920ce3a853dfccd477d890aad45bca2d13
kostyscp <5purid83@solent.ac.uk>
2023-03-15 19:45:29 +0000

    Created confirm methods for each character type

--- starter/script.js
+++ starter/script.js
@@ -90,15 +90,30 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  var passwordLenght = 0
+  var passwordLenght = 0;
   while (passwordLenght < 8 || passwordLenght > 128) {
-    passwordLenght = prompt("How long would you like the password to be? (8 - 128 characters)")
+    passwordLenght = prompt(
+      "How long would you like the password to be? (8 - 128 characters)"
+    );
     if (passwordLenght < 8) {
       console.log("Password too short!");
     } else if (passwordLenght > 128) {
       console.log("Password too long!");
     }
   }
+  var lower = confirm(
+    "Would you like your password to include lowercase characters? (Press 'OK' if you do and 'CANCEL' if you don't.)"
+  );
+  var upper = confirm(
+    "Would you like your password to include uppercase characters? (Press 'OK' if you do and 'CANCEL' if you don't.)"
+  );
+  var numeric = confirm(
+    "Would you like your password to include numeric characters? (Press 'OK' if you do and 'CANCEL' if you don't.)"
+  );
+  var special = confirm(
+    "Would you like your password to include special characters? (Press 'OK' if you do and 'CANCEL' if you don't.)"
+  );
+  
 }
 getPasswordOptions();
 // Function for getting a random element from an array
